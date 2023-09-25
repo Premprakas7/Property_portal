b0223ff5bc94378492e5f0d30242f3b051f1a371
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-25 16:16:42 +0530

    home page updated

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -5,8 +5,29 @@ const Home = () => {
   return (
     <div>
       <Header/>
-      <div>
-        
+      <div className='flex gap-[2rem]'>
+        <div>
+          <p className='font-[800]'>City</p>
+          <select  id="">
+            <option value="">City</option>
+            <option value="">City</option>
+          </select>
+        </div>
+        <div></div>
+        <div></div>
+        <div>
+        <p className='font-[800]'>City</p>
+          <select  id="">
+            <option value="">City</option>
+            <option value="">City</option>
+          </select>
+        </div>
+        <div>
+          <button className='bg-[#03071e] text-[white] w-[8rem] h-[3rem] rounded-md'>
+            Apply
+          </button>
+        </div>
+
       </div>
     </div>
   )
