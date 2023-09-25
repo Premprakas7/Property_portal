4d9c793f68329e799bea9509337626092c63b1e9
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-25 18:24:17 +0530

    models added

---
+++ Backend/src/models/property.model.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+
+const propertySchemma=mongoose.Schema({
+    name:{type:String, required:true},
+    price:{type:String, required:true},
+    city:{type:String, required:true},
+    property_type:{type:String, required:true},
+    available:{type:String, required:true},
+    img:{type:String, required:true}
+},
+{
+    versionKey:false,
+    timestamps:true
+})
+
+module.exports=mongoose.model("property",propertySchemma )
